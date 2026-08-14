ae1cb6804e5020bedae9d64b3234f35a3cc7a77a
Oliver Verdesseldonck <oliver.verdesseldonck@gmail.com>
2026-08-14 21:50:19 +0200

    Skills in the domain

---
+++ Domain/Portfolio/SkillLevel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Portfolio;
+
+public enum SkillLevel
+{
+    Beginner = 1,
+    Intermediat,
+    Advanced
+}
