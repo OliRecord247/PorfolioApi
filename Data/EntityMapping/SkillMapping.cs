00071987e25ecd4670d390b713f61f449673a601
Oliver Verdesseldonck <oliver.verdesseldonck@gmail.com>
2026-08-22 10:22:52 +0200

    Update migrations with changes

--- Data/EntityMapping/SkillMapping.cs
+++ Data/EntityMapping/SkillMapping.cs
@@ -1,9 +1,11 @@
 ﻿using Domain.Portfolio;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
 
 namespace Data.EntityMapping;
 
@@ -24,9 +26,29 @@ public class SkillMapping : IEntityTypeConfiguration<Skill>
             .Property(x => x.Level)
             .IsRequired();
 
+        builder
+            .Property(x => x.Tags)
+            .HasConversion(
+                v => JsonSerializer.Serialize(v),
+                v => JsonSerializer.Deserialize<List<string>>(v) ?? new List<string>()
+            )
+            .HasColumnType("jsonb")
+            .Metadata
+            .SetValueComparer(new ValueComparer<List<string>>(
+                    (c1, c2) => c1.SequenceEqual(c2),
+                    c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
+                    c => c.ToList()
+                ));
+
         builder.HasData(
-            new Skill { Id = 1, Name = "Vue", Level = SkillLevel.Advanced },
-            new Skill { Id = 2, Name = "React", Level = SkillLevel.Beginner }
+            new Skill { Id = 1, Name = "Vue", Image = "vue.svg", Level = SkillLevel.Advanced, Tags = ["frontend"] },
+            new Skill { Id = 2, Name = "React", Image = "react.svg", Level = SkillLevel.Beginner, Tags = ["fronted"] },
+            new Skill { Id = 3, Name = "TypeScript", Image = "typescript.svg", Level = SkillLevel.Advanced, Tags = ["fullstack", "api"] },
+            new Skill { Id = 4, Name = "Tailwind", Image = "tailwind.svg", Level = SkillLevel.Intermediat, Tags = ["fronted"] },
+            new Skill { Id = 5, Name = ".NET", Image = "dotnet.svg", Level = SkillLevel.Beginner, Tags = ["fullstack", "api"] },
+            new Skill { Id = 6, Name = "Docker", Image = "docker.svg", Level = SkillLevel.Intermediat, Tags = ["backend"] },
+            new Skill { Id = 7, Name = "MongoDB", Image = "mongodb.svg", Level = SkillLevel.Advanced, Tags = ["backend", "database"] },
+            new Skill { Id = 8, Name = "PostgreSQL", Image = "postgresql.svg", Level = SkillLevel.Intermediat, Tags = ["backend", "database"] }
         );
     }
 }
---
+++ Data/Migrations/20260819141428_ExtraSkillData.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtraSkillData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Skills",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tags",
+                table: "Skills",
+                type: "jsonb",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Image", "Tags" },
+                values: new object[] { "vue.svg", "[\"frontend\"]" });
+
+            migrationBuilder.UpdateData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Image", "Tags" },
+                values: new object[] { "react.svg", "[\"fronted\"]" });
+
+            migrationBuilder.InsertData(
+                table: "Skills",
+                columns: new[] { "Id", "Image", "Level", "Name", "Tags" },
+                values: new object[,]
+                {
+                    { 3, "typescript.svg", 3, "TypeScript", "[\"fullstack\",\"api\"]" },
+                    { 4, "tailwind.svg", 2, "Tailwind", "[\"fronted\"]" },
+                    { 5, "dotnet.svg", 1, ".NET", "[\"fullstack\",\"api\"]" },
+                    { 6, "docker.svg", 2, "Docker", "[\"backend\"]" },
+                    { 7, "mongodb.svg", 3, "MongoDB", "[\"backend\",\"database\"]" },
+                    { 8, "postgresql.svg", 2, "PostgreSQL", "[\"backend\",\"database\"]" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Skills");
+
+            migrationBuilder.DropColumn(
+                name: "Tags",
+                table: "Skills");
+        }
+    }
+}
