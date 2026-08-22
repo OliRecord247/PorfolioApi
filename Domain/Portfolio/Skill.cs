5bfdb93ba36238e100f89050bd54515156540659
Oliver Verdesseldonck <oliver.verdesseldonck@gmail.com>
2026-08-22 10:22:40 +0200

    Updating model data

--- Domain/Portfolio/Skill.cs
+++ Domain/Portfolio/Skill.cs
@@ -7,6 +7,8 @@ namespace Domain.Portfolio;
 public class Skill
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public required string Name { get; set; }
     public SkillLevel Level { get; set; }
+    public required string Image { get; set; }
+    public List<string> Tags { get; set; } = [];
 }
